c6374ef8d60a64c612a99922e5e9f86b391eeaea
vinkaannisa <vinkaannisaar@gmail.com>
2022-08-24 04:06:23 +0700

    Task Poin 4

---
+++ Hitung/Hitung.go
@@ -0,0 +1,5 @@
+package hitung
+
+func Tambah(angka1 int, angka2 int) int {
+	return angka1 + angka2
+}
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"ALterra/hitung"
+)
 
 func main() {
 	fmt.Println("Belajar UIUX di Alterra Academy MBKM MSIB Batch 3")
+	fmt.Println("fitur1 point 4")
+
+	hasilTambah := hitung.Tambah(9, 9)
+	fmt.Println(hasilTambah)
 }
